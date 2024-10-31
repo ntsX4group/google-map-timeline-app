25bccbb4c2777dadd50b1aecf3359a82f7092568
misaka mikoto <tindh.484@gmail.com>
2024-10-31 14:56:55 +0700

    create UI and base behavior for button

--- google-map-timeline-app/composeApp/src/commonMain/kotlin/kokorowa/timeline/App.kt
+++ google-map-timeline-app/composeApp/src/commonMain/kotlin/kokorowa/timeline/App.kt
@@ -1,35 +1,92 @@
 package kokorowa.timeline
 
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
-import google_map_timeline_app.composeapp.generated.resources.Res
-import google_map_timeline_app.composeapp.generated.resources.compose_multiplatform
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
+        var attendancing by remember { mutableStateOf(false) }
+        var breaking by remember { mutableStateOf(false) }
+
         Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
+            AnimatedVisibility(!attendancing) {
                 Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
+                    Box(Modifier.fillMaxWidth().padding(top = 50.dp, bottom = 20.dp), contentAlignment = Alignment.Center) {
+                        Button(onClick = { attendancing = !attendancing },
+                            colors = ButtonDefaults.buttonColors(backgroundColor = Color(255, 138, 141)),
+                            modifier = Modifier.height(100.dp).width(250.dp),
+                            shape = RoundedCornerShape(10)) {
+                            Text("出勤", color = Color.White, fontSize = 30.sp)
+                        }
+                    }
+                    Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+                        Text("訪問の場合は１件目の訪問先")
+                        Text("事務所や業務開始場所に到着時")
+                        Text("出勤ボタンを押してください")
+                    }
+                }
+            }
+            AnimatedVisibility(attendancing) {
+                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+                    AnimatedVisibility(!breaking) {
+                        Box(Modifier.fillMaxWidth().padding(top = 50.dp, bottom = 20.dp), contentAlignment = Alignment.Center) {
+                            Button(onClick = { breaking = !breaking },
+                                colors = ButtonDefaults.buttonColors(backgroundColor = Color(41, 201, 150)),
+                                modifier = Modifier.height(100.dp).width(250.dp),
+                                shape = RoundedCornerShape(10)) {
+                                Text("私用・休憩", color = Color.White, fontSize = 30.sp)
+                            }
+                        }
+                    }
+                    AnimatedVisibility(breaking) {
+                        Box(Modifier.fillMaxWidth().padding(top = 50.dp, bottom = 20.dp), contentAlignment = Alignment.Center) {
+                            Button(onClick = { breaking = !breaking },
+                                colors = ButtonDefaults.buttonColors(backgroundColor = Color(41,201,150)),
+                                modifier = Modifier.height(100.dp).width(250.dp),
+                                shape = RoundedCornerShape(10)) {
+
+                                Column(Modifier.fillMaxWidth(),
+                                    horizontalAlignment = Alignment.CenterHorizontally) {
+                                    Text("私用・休憩", color = Color.White, fontSize = 30.sp)
+                                    Text("終了", color = Color.White, fontSize = 30.sp)
+                                }
+                            }
+                        }
+                    }
+                    Box(Modifier.fillMaxWidth().padding(top = 30.dp, bottom = 20.dp), contentAlignment = Alignment.Center) {
+                        Button(onClick = { attendancing = !attendancing },
+                            colors = ButtonDefaults.buttonColors(backgroundColor = Color(80, 161, 223)),
+                            modifier = Modifier.height(100.dp).width(250.dp),
+                            shape = RoundedCornerShape(10),
+                            enabled = !breaking) {
+                            Text("退勤", color = Color.White, fontSize = 30.sp)
+                        }
+                    }
+                    Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+                        Text("訪問の場合は最後の訪問先終了時")
+                        Text("事務所や業務終了時点で")
+                        Text("退勤ボタンを押してください")
+                    }
                 }
             }
         }
