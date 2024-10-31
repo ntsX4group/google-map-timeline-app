417f79ae2bf69e5211a9a740b2b353afdb592cd1
misaka mikoto <tindh.484@gmail.com>
2024-10-31 18:47:31 +0700

    update UI

--- google-map-timeline-app/composeApp/src/commonMain/kotlin/kokorowa/timeline/App.kt
+++ google-map-timeline-app/composeApp/src/commonMain/kotlin/kokorowa/timeline/App.kt
@@ -1,23 +1,34 @@
 package kokorowa.timeline
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import google_map_timeline_app.composeapp.generated.resources.Res
+import google_map_timeline_app.composeapp.generated.resources.logo
+import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 
@@ -29,6 +40,32 @@ fun App() {
         var breaking by remember { mutableStateOf(false) }
 
         Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+            Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+                Box(Modifier.fillMaxWidth().drawBehind {
+                    val strokeWidth = 1.dp.toPx() * density
+                    val y = size.height - strokeWidth / 2
+                    drawLine(
+                        Color.LightGray,
+                        Offset(0f, y + 10 ),
+                        Offset(size.width, y),
+                        strokeWidth
+                    )
+                }, contentAlignment = Alignment.TopStart) {
+                    Box(Modifier.fillMaxWidth().padding(top = 5.dp, bottom = 5.dp, start = 10.dp)) {
+                        Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Start, verticalAlignment = Alignment.CenterVertically) {
+                            Icon(
+                                modifier = Modifier.size(50.dp),
+                                painter = painterResource(Res.drawable.logo),
+                                contentDescription = "Some icon",
+                                tint = Color.Unspecified
+                            )
+                            Text("c", color = Color.Red, fontSize = 30.sp)
+                            Text("ocorowa", color = Color.Gray, fontSize = 30.sp)
+                            Text("勤怠", color = Color.Gray, fontSize = 30.sp)
+                        }
+                    }
+                }
+            }
             AnimatedVisibility(!attendancing) {
                 Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
                     Box(Modifier.fillMaxWidth().padding(top = 50.dp, bottom = 20.dp), contentAlignment = Alignment.Center) {
